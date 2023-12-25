6dca5c7c5e291553823082ebdb5d445463a84507
mdarifmia <arif@supremesupports.com.au>
2023-12-25 21:09:32 +0600

    php variable declare syntax

--- syntax_variable.php
+++ syntax_variable.php
@@ -1,3 +1,14 @@
+<!-- php syntax -->
 <?php 
-echo "This is PHP syntax";
+// echo "This is PHP syntax";
+
+// echo " Md. Arif Mia";
+?>
+
+<?php 
+$name = "Md. Arif ";
+$age = 24 ;
+$profession = " Software Engineer";
+
+echo $name, $age, $profession;
 ?>
