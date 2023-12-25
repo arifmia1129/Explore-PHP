71029f31a8e3c71811065a048e4c6e666fac8af0
mdarifmia <arif@supremesupports.com.au>
2023-12-25 21:25:13 +0600

    template string

--- syntax_variable.php
+++ syntax_variable.php
@@ -10,5 +10,15 @@ $name = "Md. Arif ";
 $age = 24 ;
 $profession = " Software Engineer";
 
-echo $name, $age, $profession;
+// echo $name, $age, $profession;
+?>
+
+<?php 
+
+$name = "Md. Arif Mia";
+$balance =  10000;
+
+echo "Hello,".$name.", your balance is ".$balance;
+echo "\nHello, $name, your current account balance is: $balance";
+
 ?>
