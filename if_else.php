55797f57b92551cbc7a00c28a4289047fccc48d3
mdarifmia <arif@supremesupports.com.au>
2023-12-26 12:02:21 +0600

    if else

---
+++ if_else.php
@@ -0,0 +1,12 @@
+<?php
+
+$user_name = "mdarifmia";
+$password = "12345678";
+
+if ($user_name === "mdarifmia" && $password === "12345678") {
+    echo "Logged in successfully.";
+} else {
+    echo "Failed to log in. Provide your user name and password correctly.";
+}
+
+?>
