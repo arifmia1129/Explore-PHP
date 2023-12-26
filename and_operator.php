d101e1e0790a0073bbbeb0df6c4180d8d63dbb5f
mdarifmia <arif@supremesupports.com.au>
2023-12-26 20:20:30 +0600

    and operator

---
+++ and_operator.php
@@ -0,0 +1,11 @@
+<?php
+$user_name = "arif";
+$password = "123456";
+
+if($user_name === "arif" && $password === "123456") {
+    echo "Logged in successfully.";
+}else{
+    echo "Doesn't exit user with your provided user name and password";
+}
+
+?>
