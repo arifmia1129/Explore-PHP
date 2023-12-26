95281d9304ba22c3f9c8c950cce502c74c37aaf5
mdarifmia <arif@supremesupports.com.au>
2023-12-26 11:37:41 +0600

    comparison operators

---
+++ comparison_operators.php
@@ -0,0 +1,15 @@
+<?php
+$value1 = "200";
+$value2 = 200;
+
+printf("%d < %d = %b\n", $value1, $value2, $value1 < $value2);
+printf("%d <= %d = %b\n", $value1, $value2, $value1 <= $value2);
+printf("%d > %d = %b\n", $value1, $value2, $value1 > $value2);
+printf("%d >= %d = %b\n", $value1, $value2, $value1 >= $value2);
+printf("%d == %d = %b\n", $value1, $value2, $value1 == $value2);
+printf("%d != %d = %b\n", $value1, $value2, $value1 != $value2);
+printf("%d != %d = %b\n", $value1, $value2, $value1 != $value2);
+printf("%d === %d = %b\n", $value1, $value2, $value1 === $value2);
+printf("%d !== %d = %b\n", $value1, $value2, $value1 !== $value2);
+
+?>
