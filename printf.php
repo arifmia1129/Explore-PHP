8f3ac47857f0535105712066acb680598d1049bf
mdarifmia <arif@supremesupports.com.au>
2023-12-26 08:28:12 +0600

    printf

--- printf.php
+++ printf.php
@@ -1,6 +1,14 @@
 <?php
 $name = "arif";
+$age = 24;
 $new_name = ucwords($name);
 
-echo "My name is: $new_name";
+// echo "My name is: $new_name";
+// echo "My name is: ".ucwords($name);
+
+$details = "Name is: %s, age is: %s";
+
+// printf("My name is %s, my age is %s", ucwords($name), $age);
+
+printf($details, ucwords($name), $age);
 ?>
