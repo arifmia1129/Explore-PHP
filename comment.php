f6fd36e1cd96f6ffbfdbb9060a1d72cc6fc42a7c
mdarifmia <arif@supremesupports.com.au>
2023-12-26 06:35:09 +0600

    learn how to do single and multiline comment in php

---
+++ comment.php
@@ -0,0 +1,17 @@
+<?php
+#Today we learn about comments in PHP
+
+// Price calculation code
+
+/*
+In this system we learn how to make comment in php. If you want to do single line comment we can use two methods. First is start your comment line with # and another is start your comment with //. If you want to do double line comment we can use / * write comment here * /
+*/
+
+$product_price = 100; //actual product price
+$vat = 10; //included vat
+
+$total_product_price = $product_price + $vat; //total price 
+
+echo "Total product price is: $total_product_price"; //print total price
+
+?>
