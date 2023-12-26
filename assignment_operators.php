d1544ddd106f02ab5f4616a3da1cc722d44b8f43
mdarifmia <arif@supremesupports.com.au>
2023-12-26 10:37:50 +0600

    assignment operators

---
+++ assignment_operators.php
@@ -0,0 +1,24 @@
+<?php
+$balance = 100;
+
+$balance += 100;
+
+echo "Total balance is: $balance\n";
+
+$balance -= 100;
+
+echo "Total balance is: $balance\n";
+
+$balance *= 100;
+
+echo "Total balance is: $balance\n";
+
+$balance /= 100;
+
+echo "Total balance is: $balance\n";
+
+$balance %= 100;
+
+echo "Total balance is: $balance\n";
+
+?>
