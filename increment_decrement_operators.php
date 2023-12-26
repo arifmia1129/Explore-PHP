a5dd01e12c4035675afc00a9a6f67c358a091f73
mdarifmia <arif@supremesupports.com.au>
2023-12-26 10:49:10 +0600

    increment and decrement operators

---
+++ increment_decrement_operators.php
@@ -0,0 +1,21 @@
+<?php
+$value = 10;
+
+// Example of post increment
+// echo $value++;
+// echo $value;
+
+
+// Example of pre increment
+// echo ++$value;
+// echo $value;
+
+// Example of post decrement
+// echo $value--;
+// echo $value;
+
+// Example of pre decrement
+echo --$value;
+echo $value;
+
+?>
