14fa14f5a374ed99f86efc1a5c16340dac158255
mdarifmia <arif@supremesupports.com.au>
2023-12-26 09:10:30 +0600

    printf

--- printf.php
+++ printf.php
@@ -10,5 +10,29 @@ $details = "Name is: %s, age is: %s";
 
 // printf("My name is %s, my age is %s", ucwords($name), $age);
 
-printf($details, ucwords($name), $age);
+// printf($details, ucwords($name), $age);
+?>
+
+<?php
+$name = "Arif";
+$age = 24;
+$points = 10.5;
+$is_married = true;
+
+// printf("Name is: %s, age: %d, points: %f, marital status: %b", $name, $age, $points, $is_married);
+
+
+$first_name = "Md. Arif";
+$last_name = "Mia";
+
+// printf('Last name is: %2$s and first name is %1$s', $first_name, $last_name);
+
+// printf('My name is: %1$s, my first name also %1$s', $first_name);
+
+$amount = 100;
+$total_amount = $amount * 9.95;
+
+// printf('Total amount is %.0f', $total_amount);
+printf("Name is [%-'@10.2s]", $name);
+
 ?>
