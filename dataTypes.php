948592ee27dd40dc245a0c90de5db18e4e831b7e
mdarifmia <arif@supremesupports.com.au>
2023-12-26 07:44:17 +0600

    var_dump for testing variable info

--- dataTypes.php
+++ dataTypes.php
@@ -3,7 +3,7 @@
 $name = "Md. Arif Mia"; //string type
 $age = 24; //number (int) type
 $points = 10.5; //number (float) type
-$isAdmin = false; //boolean type
+$is_admin = false; //boolean type
 $balance = null; //null type
 
 ?>
---
+++ var_dump.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Md. Arif Mia";
+$age = 24;
+$is_married = true;
+
+var_dump($name, $age, $is_married);
+?>
