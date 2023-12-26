6248214803556e95e47df5aec23e47b582e0e43e
mdarifmia <arif@supremesupports.com.au>
2023-12-26 11:00:35 +0600

    concatenation

---
+++ concatenation.php
@@ -0,0 +1,20 @@
+<?php
+$first_name = "Md. Arif";
+$last_name = "Mia";
+
+$full_name = $first_name . " " . $last_name;
+
+$getting = "Hello, " . $first_name . " " . $last_name;
+
+// echo $getting;
+
+
+$institute_name = "Vocational Teachers Training Institute";
+$address = "Bogura";
+
+$institute_name .= " " . $address;
+
+
+echo $institute_name;
+
+?>
