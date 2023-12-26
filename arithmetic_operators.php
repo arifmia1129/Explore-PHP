feeff50cc6d1d1eda8105a1a722727e166c2a6fe
mdarifmia <arif@supremesupports.com.au>
2023-12-26 09:21:29 +0600

    arithmetic operators

---
+++ arithmetic_operators.php
@@ -0,0 +1,13 @@
+<?php
+$first_num = 10;
+$second_num = 5;
+
+
+printf("%d + %d = %d\n", $first_num, $second_num, $first_num + $second_num);
+printf("%d - %d = %d\n", $first_num, $second_num, $first_num - $second_num);
+printf("%d x %d = %d\n", $first_num, $second_num, $first_num * $second_num);
+printf("%d / %d = %d\n", $first_num, $second_num, $first_num / $second_num);
+printf("%d  %d = %d\n", $first_num, $second_num, $first_num % $second_num);
+printf("%d ** %d = %d\n", $first_num, $second_num, $first_num ** $second_num);
+
+?>
