963fd42fb76429c3b23b1d2a672eaed2abcd6658
mdarifmia <arif@supremesupports.com.au>
2023-12-26 07:28:00 +0600

    data types

---
+++ dataTypes.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Md. Arif Mia"; //string type
+$age = 24; //number (int) type
+$points = 10.5; //number (float) type
+$isAdmin = false; //boolean type
+$balance = null; //null type
+
+?>
