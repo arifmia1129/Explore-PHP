de0d33007617dc7b0132b535c1ec3193976d73ee
mdarifmia <arif@supremesupports.com.au>
2023-12-27 07:47:03 +0600

    switch case

---
+++ switch_case.php
@@ -0,0 +1,30 @@
+<?php
+
+    $letter = "O";
+
+    switch($letter) {
+        case "a":
+        case "A":
+            echo "Vowel";
+            break;
+        case "e":
+        case "E":
+            echo "Vowel";
+            break;
+        case "i":
+        case "I":
+            echo "Vowel";
+            break;
+        case "o":
+        case "O":
+            echo "Vowel";
+            break;
+        case "u":
+        case "U":
+            echo "Vowel";
+            break;
+        default:
+            echo "Consonant";
+    }
+
+?>
