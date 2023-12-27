68cb1efeae531f0a176dc5ba3dc60b56d9f0f660
mdarifmia <arif@supremesupports.com.au>
2023-12-27 12:38:49 +0600

    function with parameter

---
+++ function_parameter.php
@@ -0,0 +1,9 @@
+<?php
+
+    function show_student_info($name, $class, $gender) {
+        echo "School Name: Supreme Supports School,\nStudent Name: $name,\nClass: $class,\nGender: $gender";
+    }
+
+    show_student_info("Arif", "Diploma In Computer Engineering", "Male");
+
+?>
