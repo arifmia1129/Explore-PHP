20a7add2c708bc9e6d9b727ad8feb78fb7b75255
mdarifmia <arif@supremesupports.com.au>
2023-12-27 21:04:38 +0600

    function return

---
+++ function_return.php
@@ -0,0 +1,9 @@
+<?php
+    function full_name($first_name, $last_name) {
+        return $first_name." ".$last_name;
+    } 
+
+    $full_name = full_name("Md. Arif", "Mia");
+
+    echo $full_name;
+?>
