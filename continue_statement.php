5c645f1be201038909923de8e14d87c8385e9707
mdarifmia <arif@supremesupports.com.au>
2023-12-27 06:52:47 +0600

    continue statement

---
+++ continue_statement.php
@@ -0,0 +1,12 @@
+<?php
+
+    for($count = 1; $count <= 10; $count++) {
+        echo "\nBangladesh\n";
+
+        if($count === 6){
+            continue;
+        }
+        echo "is my country";
+    }
+
+?>
